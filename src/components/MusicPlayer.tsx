0145052036e203bb37e570e4447cd2e3fbaa317e
pvitto98 <pvitto98>
2024-04-19 14:59:32 +0000

    Pushed from Locofy Builder

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -6,15 +6,21 @@ const MusicPlayer: FunctionComponent = () => {
     <div className={styles.musicplayer}>
       <img className={styles.imageIcon} alt="" src="/image@2x.png" />
       <div className={styles.playercontent}>
-        <div className={styles.content}>
-          <div className={styles.contenttext}>
-            <div className={styles.copywriting}>COPYWRITING</div>
-            <div className={styles.tiScrivoLe}>ti scrivo le cose</div>
-          </div>
-          <div className={styles.contentcontrol}>
-            <img className={styles.backIcon} alt="" src="/back.svg" />
-            <img className={styles.buttonIcon} alt="" src="/button.svg" />
-            <img className={styles.backIcon} alt="" src="/forward.svg" />
+        <div className={styles.contentWrapper}>
+          <div className={styles.content}>
+            <div className={styles.contenttext}>
+              <div className={styles.socialMediaMarketing}>
+                SOCIAL MEDIA MARKETING
+              </div>
+              <div className={styles.seISocial}>se i social fossero musica</div>
+            </div>
+            <div className={styles.contentcontrol}>
+              <img className={styles.backIcon} alt="" src="/back1.svg" />
+              <div className={styles.vectorWrapper}>
+                <img className={styles.vectorIcon} alt="" src="/vector3.svg" />
+              </div>
+              <img className={styles.backIcon} alt="" src="/forward1.svg" />
+            </div>
           </div>
         </div>
       </div>
